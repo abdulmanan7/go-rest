342d54883c631720038a5b624570a9d53455f517
Abdul Manan <abdulmanan@troontechnologies.com>
2020-02-13 13:07:59 +0500

    post module added

--- api/controllers/base.go
+++ api/controllers/base.go
@@ -27,24 +27,24 @@ func (server *Server) Initialize(Dbdriver, DbUser, DbPassword, DbPort, DbHost, D
 		DBURL := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local", DbUser, DbPassword, DbHost, DbPort, DbName)
 		server.DB, err = gorm.Open(Dbdriver, DBURL)
 		if err != nil {
-			fmt.Printf("Cannot connect to %s database ", Dbdriver)
+			fmt.Printf("Cannot connect to %s database", Dbdriver)
 			log.Fatal("This is the error:", err)
 		} else {
-			fmt.Printf("We are connected to the %s database ", Dbdriver)
+			fmt.Printf("We are connected to the %s database", Dbdriver)
 		}
 	}
 	if Dbdriver == "postgres" {
 		DBURL := fmt.Sprintf("host=%s port=%s user=%s dbname=%s sslmode=disable password=%s", DbHost, DbPort, DbUser, DbName, DbPassword)
 		server.DB, err = gorm.Open(Dbdriver, DBURL)
 		if err != nil {
-			fmt.Printf("Cannot connect to %s database ", Dbdriver)
+			fmt.Printf("Cannot connect to %s database", Dbdriver)
 			log.Fatal("This is the error:", err)
 		} else {
-			fmt.Printf("We are connected to the %s database ", Dbdriver)
+			fmt.Printf("We are connected to the %s database", Dbdriver)
 		}
 	}
 
-	server.DB.Debug().AutoMigrate(&models.User{}) //database migration
+	server.DB.Debug().AutoMigrate(&models.User{}, &models.Post{}) //database migration
 
 	server.Router = mux.NewRouter()
 
