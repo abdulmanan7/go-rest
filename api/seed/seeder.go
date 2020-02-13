41f18fbed63028b2ff197463e5a86d2aae9cacf8
Abdul Manan <abdulmanan@troontechnologies.com>
2020-02-13 12:22:24 +0500

    project init

---
+++ api/seed/seeder.go
@@ -0,0 +1,40 @@
+package seed
+
+import (
+	"log"
+
+	"github.com/abdulmanan7/go-rest/api/models"
+	"github.com/jinzhu/gorm"
+)
+
+var users = []models.User{
+	models.User{
+		Nickname: "Steven victor",
+		Email:    "steven@gmail.com",
+		Password: "password",
+	},
+	models.User{
+		Nickname: "Martin Luther",
+		Email:    "luther@gmail.com",
+		Password: "password",
+	},
+}
+
+func Load(db *gorm.DB) {
+
+	err := db.Debug().DropTableIfExists(&models.User{}).Error
+	if err != nil {
+		log.Fatalf("cannot drop table: %v", err)
+	}
+	err = db.Debug().AutoMigrate(&models.User{}).Error
+	if err != nil {
+		log.Fatalf("cannot migrate table: %v", err)
+	}
+
+	for i, _ := range users {
+		err = db.Debug().Model(&models.User{}).Create(&users[i]).Error
+		if err != nil {
+			log.Fatalf("cannot seed users table: %v", err)
+		}
+	}
+}
